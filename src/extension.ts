1086dd9b9169d3138401e839a15518f2989a5259
Wesley Nepomuceno <wesleynepomuceno1@gmail.com>
2020-12-17 22:03:45 -0300

    Atualizada versão corrigida compilação

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,13 @@
 import * as vscode from 'vscode';
 import {compilador} from './modules/CompilaProduto'
 import {exportaArquivo, exportaTodosArquivos} from './modules/ExportaArquivo'
-import {pegaIndices, test} from './modules/CarregaIndices'
+import {pegaIndices} from './modules/CarregaIndices'
 
 export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(exportaArquivo);
 	context.subscriptions.push(exportaTodosArquivos);
 	context.subscriptions.push(compilador);
-	context.subscriptions.push(test);
 	context.subscriptions.push(pegaIndices)
 }
 
